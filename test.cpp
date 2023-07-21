688e811da17ec7de25345bd669da5c098a98eee0
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-07-21 20:59:05 +0600

    2

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+//                 vector<std::string> str = {"zbc",      "bbcd", "bajk", "bs"};
+//                sort(str.begin(),str.end());
+//                for (auto& element : str) {
+//                    std::cout << element << " ";
+//                }
+//                std::cout << std::endl;
+
+    string sum = "as";
+    string a = "ab";
+    string b = "ir";
+
+sum = a + b;
+cout << sum;
+
+ return 0;
+}
