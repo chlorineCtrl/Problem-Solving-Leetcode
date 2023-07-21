c52b6e289dfbd0058840cd60c39e38d2923ce9a7
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-07-21 19:48:53 +0600

    start

---
+++ problems.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/roman-to-integer/
+class Solution {
+public:
+    int romanToInt(string romanInput) {
+        int count = 0;
+        unordered_map<char, int> map;
+
+        map['I'] = 1;
+        map['V'] = 5;
+        map['X'] = 10;
+        map['L'] = 50;
+        map['C'] = 100;
+        map['D'] = 500;
+        map['M'] = 1000;
+
+        for(int i = 0; i< romanInput.size(); i++){
+            if(map[romanInput[i]] < map[romanInput[i+1]]){
+                count -= map[romanInput[i]];
+            }
+            else
+            {
+                count += map[romanInput[i]];
+            }
+        }
+        
+        return count;
+    }
+};
