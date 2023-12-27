049a81877c85714452f697ff5690ea81ba26656e
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-12-27 22:02:50 +0600

    problem 10

--- problems.cpp
+++ problems.cpp
@@ -204,3 +204,22 @@ public:
 
    
 };
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int buyPrice = INT_MAX; //max possible integer
+        int maxProfit = 0;
+
+        for(int i = 0; i < n ; i++){
+            buyPrice = min(buyPrice,prices[i]);
+            maxProfit = max(maxProfit,prices[i]-buyPrice);
+        }
+        return maxProfit;
+
+    }
+};
+
