0f25d975a12480848304b768d03b178d850eaaff
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-12-27 22:36:48 +0600

    problem 12

--- problems.cpp
+++ problems.cpp
@@ -236,3 +236,14 @@ public:
     }
 };
 
+//https://leetcode.com/problems/majority-element/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+    }
+};
+
+
