820dd8d95c118c4e5e2f78528f7f017a726a79ec
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-12-30 16:37:08 +0600

    problem 14

--- problems.cpp
+++ problems.cpp
@@ -266,4 +266,19 @@ public:
     }
 };
 
+//https://leetcode.com/problems/invert-binary-tree/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root) {
+            std::swap(root->left, root->right);
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
+    }
+};
+
+
 
