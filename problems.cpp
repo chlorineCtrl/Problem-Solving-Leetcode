5cd2321cb865edbb89c5efc726c080ce13506f4c
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2023-12-30 17:35:45 +0600

    problem 15

--- problems.cpp
+++ problems.cpp
@@ -329,5 +329,25 @@ public:
     }
 };
 
+//https://leetcode.com/problems/move-zeroes/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+
+        for(int i=0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for(j;j<nums.size();j++){
+            nums[j] = 0;
+        }
+        
+    }
+};
+
 
 
