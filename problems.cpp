2d8261f194436dbdf93592e9be9511dcd3e6d3f4
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-02-01 18:33:36 +0600

    28

--- problems.cpp
+++ problems.cpp
@@ -581,4 +581,19 @@ public:
     }
 };
 
+//https://leetcode.com/problems/jewels-and-stones/
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int ans = 0;
+        for(int i = 0; i< jewels.length(); i++){
+            for(int j = 0; j< stones.length(); j++ ){
+                if(jewels[i]==stones[j]) ans++;
+            }
+        }
+        return ans;
+    }
+};
+
 
