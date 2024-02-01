77456ce839baac7ffc153f7d4fcf71aa7998e457
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-02-01 18:26:25 +0600

    26

--- problems.cpp
+++ problems.cpp
@@ -567,4 +567,18 @@ public:
     }
 };
 
+//https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for(auto i: operations){
+            if(i == "--X" || i == "X--") ans--;
+            else if(i == "++X" || i == "X++") ans++;
+        }
+        return ans;
+    }
+};
+
 
