d8bde5c10793a320d09a4caac2c8500ba1783d2f
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-02-15 19:08:37 +0600

    33(stack/queue/linkedlist practice)

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int main(){
+
+    queue<int> qu;
+    qu.push(1);
+    qu.push(2);
+    qu.push(3);
+
+    cout << qu.front();
+    cout << qu.back();
+    cout << "\n";
+
+    while(!qu.empty()){
+        cout << qu.front();
+        qu.pop();
+    }
+
+}
+
+
+/*#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> myStack;
+
+    myStack.push(1);
+    myStack.push(2);
+
+    cout << myStack.top();
+
+    while(!myStack.empty()){
+    cout << myStack.top();
+    myStack.pop();
+    }
+}
+
+
+//linked list
+#include<bits/stdc++.h>
+using namespace std;
+
 int main()
 {
     class Node
@@ -27,4 +68,4 @@ int main()
         head = head->next;
     }
 
-}
+}*/
