db4efe300e957ec2f0eb3935be9f23e4a315972d
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-06-18 18:45:59 +0600

    44

--- problems.cpp
+++ problems.cpp
@@ -893,3 +893,33 @@ public:
     return ans;
     }
 };
+
+//https://leetcode.com/problems/valid-sudoku/
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool row[9][9] = {false};
+        bool col[9][9] = {false};
+        bool sub[9][9] = {false};
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+
+                int val = board[i][j]-'0'-1;
+                int area = (i/3)*3+(j/3);
+                //check
+                if( row[i][val] || col[j][val] || sub[area][val]){
+                    return false;
+                }
+
+                row[i][val] = true;
+                col[j][val] = true;
+                sub[area][val] = true;
+
+            }
+        }
+        return true;
+    }
+};
