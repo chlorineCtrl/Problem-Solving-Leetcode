d34a22c3a8074aa874a8c6ac5c33431871d0438c
chlorineCtrl <abirhasan5@iut-dhaka.edu>
2024-10-14 22:30:22 +0600

    WellDev

---
+++ WellDev/q1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int ara[4]={2, 4, 1, 8};
+    printf("%p",ara); // In C, the array name (ara) essentially acts as a pointer to the first element of the array
+}
+
+
+
