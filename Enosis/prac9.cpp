f43cbf000390fcc8fe400abd35734ea75cd9e864
Abir Hasan Fuad <abirhasan5@iut-dhaka.edu>
2025-02-05 22:29:47 +0600

    enosisprac

---
+++ Enosis/prac9.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    vector<int> arr = {1,3,1,9,5};
+
+    sort(arr.begin(), arr.end(), greater<>());
+
+    cout << arr[0];
+
+
+
+}
