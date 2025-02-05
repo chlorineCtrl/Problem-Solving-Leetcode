831d297a5b31ce6523620c8ca176f34de77d498d
Abir Hasan Fuad <abirhasan5@iut-dhaka.edu>
2025-02-06 01:24:18 +0600

    enosisprac

--- Enosis/prac9.cpp
+++ Enosis/prac9.cpp
@@ -1,15 +1,32 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
+void printCenteredTriangle(int baseWidth) {
+    int maxWidth = baseWidth * 2 - 1; // Full width needed for centering
 
+    for (int i = 1; i <= baseWidth; i++) {
+        int numCount = i; // Number of numbers in the row
+        int spaceCount = (maxWidth - (numCount * 2 - 1)) / 2; // Center alignment spaces
 
-int main(){
-    vector<int> arr = {1,3,1,9,5};
+        // Print leading spaces for centering
+        for (int j = 0; j < spaceCount; j++) {
+            std::cout << " ";
+        }
 
-    sort(arr.begin(), arr.end(), greater<>());
+        // Print numbers
+        for (int j = 1; j <= numCount; j++) {
+            std::cout << j<< " ";
+        }
 
-    cout << arr[0];
+        std::cout << std::endl;
+    }
+}
 
+int main() {
+    int baseWidth;
+    std::cout << "Enter the base width of the triangle: ";
+    std::cin >> baseWidth;
 
+    printCenteredTriangle(baseWidth);
 
+    return 0;
 }
